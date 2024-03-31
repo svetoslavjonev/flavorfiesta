74242f728c11e37d23162fb6531d108ea34c78ac
SJ <svetoslav.jonev@abv.bg>
2024-03-31 05:50:42 +0000

    updated item details based on the occasion model

--- django_project/chefs/views.py
+++ django_project/chefs/views.py
@@ -3,9 +3,11 @@ from django.urls import reverse_lazy
 from django.views import generic as views
 from django.contrib.auth import mixins as auth_mixin
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 from django_project.chefs.models import Chef
 from django_project.chefs.forms import ChefForm
+from django_project.occasions.models import Occasion
 
 
 class ChefDashboardView(auth_mixin.LoginRequiredMixin, views.TemplateView):
@@ -17,6 +19,7 @@ class ChefDashboardView(auth_mixin.LoginRequiredMixin, views.TemplateView):
 		context['object_list'] = Chef.objects.order_by('pk')
 		return context
 
+
 class ChefDetailsView(auth_mixin.LoginRequiredMixin, views.TemplateView):
 	template_name = 'chefs/chef-details.html'
 
@@ -24,7 +27,12 @@ class ChefDetailsView(auth_mixin.LoginRequiredMixin, views.TemplateView):
 		context = super().get_context_data(**kwargs)
 		chef_id = self.kwargs.get('pk')
 		context['chef'] = get_object_or_404(Chef, pk=chef_id)
+		context['next_occasions'] = Occasion.objects.filter(
+			chef=context['chef'], 
+			start_time__gte=timezone.now()
+		).order_by('start_time')[:5]
 		return context
+
 
 class ChefCreateView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredMixin, views.CreateView):
 	permission_required = 'chefs.add_chef'
@@ -34,6 +42,7 @@ class ChefCreateView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequire
 	def get_success_url(self):
 		return reverse_lazy('chef-details', kwargs={'pk': self.object.pk, 'slug': self.object.slug})
 
+
 class ChefEditView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredMixin, views.UpdateView):
 	permission_required = 'chefs.change_chef'
 	model = Chef
@@ -42,6 +51,7 @@ class ChefEditView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredM
 
 	def get_success_url(self):
 		return reverse_lazy('chef-details', kwargs={'pk': self.object.pk, 'slug': self.object.slug})
+
 
 class ChefDeleteView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredMixin, views.DeleteView):
 	permission_required = 'chefs.delete_chef'
--- django_project/venues/views.py
+++ django_project/venues/views.py
@@ -3,9 +3,11 @@ from django.urls import reverse_lazy
 from django.views import generic as views
 from django.contrib.auth import mixins as auth_mixin
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 from django_project.venues.models import Venue
 from django_project.venues.forms import VenueForm
+from django_project.occasions.models import Occasion
 
 
 class VenueDashboardView(auth_mixin.LoginRequiredMixin, views.TemplateView):
@@ -17,6 +19,7 @@ class VenueDashboardView(auth_mixin.LoginRequiredMixin, views.TemplateView):
 		context['object_list'] = Venue.objects.order_by('pk')
 		return context
 
+
 class VenueDetailsView(auth_mixin.LoginRequiredMixin, views.TemplateView):
 	template_name = 'venues/venue-details.html'
 
@@ -24,7 +27,12 @@ class VenueDetailsView(auth_mixin.LoginRequiredMixin, views.TemplateView):
 		context = super().get_context_data(**kwargs)
 		venue_id = self.kwargs.get('pk')
 		context['venue'] = get_object_or_404(Venue, pk=venue_id)
+		context['next_occasions'] = Occasion.objects.filter(
+			venue=context['venue'],
+			start_time__gte=timezone.now()
+		).order_by('start_time')[:5]
 		return context
+
 
 class VenueCreateView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredMixin, views.CreateView):
 	permission_required = 'venues.add_venue'
@@ -34,6 +42,7 @@ class VenueCreateView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequir
 	def get_success_url(self):
 		return reverse_lazy('venue-details', kwargs={'pk': self.object.pk, 'slug': self.object.slug})
 
+
 class VenueEditView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredMixin, views.UpdateView):
 	permission_required = 'venues.change_venue'
 	model = Venue
@@ -42,6 +51,7 @@ class VenueEditView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequired
 
 	def get_success_url(self):
 		return reverse_lazy('venue-details', kwargs={'pk': self.object.pk, 'slug': self.object.slug})
+
 
 class VenueDeleteView(auth_mixin.LoginRequiredMixin, auth_mixin.PermissionRequiredMixin, views.DeleteView):
 	permission_required = 'venues.delete_venue'
