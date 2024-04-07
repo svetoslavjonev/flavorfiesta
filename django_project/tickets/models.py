bbc51fe291514a50d702b88337012cad2fd7834a
SJ <svetoslav.jonev@abv.bg>
2024-04-07 09:00:05 +0000

    refactor

--- django_project/tickets/models.py
+++ django_project/tickets/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django_project.occasions.models import Occasion
-import uuid
 
 class Ticket(models.Model):
 	occasion = models.ForeignKey(
