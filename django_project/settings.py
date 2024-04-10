a51780bbaf7d09c6c299a82d769e0c37dec118d8
SJ <svetoslav.jonev@abv.bg>
2024-04-10 13:33:57 +0000

    serve static in prod

--- django_project/settings.py
+++ django_project/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,9 +142,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
 	BASE_DIR / "staticfiles",
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
---
+++ static/js/occasion.cf81f2490580.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.occasion-form input, .occasion-form select').forEach(function(input) {
+        input.addEventListener('change', function() {
+            document.getElementById('occasionForm').submit();
+        });
+    });
+});
