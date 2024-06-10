28af7f9b49e524551214dd689331639f6a5ef8d1
SJ <svetoslav.jonev@abv.bg>
2024-06-10 09:46:20 +0000

    removed static for deployment

--- static/js/occasion.cf81f2490580.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.occasion-form input, .occasion-form select').forEach(function(input) {
-        input.addEventListener('change', function() {
-            document.getElementById('occasionForm').submit();
-        });
-    });
-});
